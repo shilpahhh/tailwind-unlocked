68f2665e24056d161f652513ba4e3981238edd38
shilpahhh <silpababu2005@gmail.com>
2025-10-31 19:28:46 +0530

    Add files via upload

---
+++ Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+
+function Nav() {
+  return (
+    <div  class="w-full   bg-gray-900 text-yellow-500 
+    py-13 shadow-lg fixed  z-50  flex items-center pb-4 justify-center gap-6 text-lg font-medium tracking-wide">
+    
+          <NavLink to="/">HOME</NavLink>
+          <NavLink to="/about">WHAT ARE WE?</NavLink>
+          <NavLink to="/launch">WHEN WE LAUNCH?</NavLink>
+          <NavLink to="/footer">SOCIALS</NavLink>
+   
+    </div>
+  )
+}
+
+export default Nav
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      keyframes: {
+        slideUp: {
+          '0%': { transform: 'translateY(40px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+      },
+      animation: {
+        slideUp: 'slideUp 1s ease-out forwards',
+      },
+    },
+  },
+  plugins: [],
+};
