de4b3b6776f511e543f83efdb82dad58a72794c3
shilpahhh <silpababu2005@gmail.com>
2025-10-31 19:35:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,40 @@
 
+import { useState } from 'react'
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { BrowserRouter, Route, Router, Routes} from 'react-router-dom';
+import Nav from '../Nav';
+import Launch from './Launch';
+import Home from './home';
+import About from './About';
+import Footer from './Footer';
+
+
+
+
+function App() {
+  
+  
+
+  return (
+    
+      <>
+      
+
+      <BrowserRouter>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/launch" element={<Launch/>}/>
+        <Route path="/footer" element={<Footer/>}/>
+      </Routes>
+     </BrowserRouter>
+    </>
+
+
+  
+  )
+}
+
+export default App
